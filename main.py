600d8f6d32277a0aa750ff5760aa8f68191e9a31
pks <pks891618@gmail.com>
2023-02-26 13:13:03 +0530

    added relatgive path

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ Bootstrap(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False, base_url=None)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = r'sqlite:///C:/Users/NIKITA/PycharmProjects/Blog-with-user/flask.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = r'sqlite:///flask.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
